9eab32ea6b781b70ddcc343ebb1fe3a69bfc0302
aadrshkashyp <aadarshkashya51@gmail.com>
2023-08-29 08:42:20 +0530

    port updated

--- src/actions/ipcActions.js
+++ src/actions/ipcActions.js
@@ -1,23 +1,23 @@
 import axios from 'axios';
 
 export const fetchIPC = () => async (dispatch) => {
-  const response = await axios.get('http://3.122.239.186:8021/api/ipc');
+  const response = await axios.get('https://3.122.239.186:5656/api/ipc');
   dispatch({ type: 'FETCH_IPC', payload: response.data });
 };
 
 export const addIPC = (ipcData) => async (dispatch) => {
-  const response = await axios.post('http://3.122.239.186:8021/api/ipc', ipcData);
+  const response = await axios.post('https://3.122.239.186:5656/api/ipc', ipcData);
   dispatch({ type: 'ADD_IPC', payload: response.data });
 };
 
 export const deleteIPC = (id) => async (dispatch) => {
-  await axios.delete(`http://3.122.239.186:8021/api/ipc/${id}`);
+  await axios.delete(`https://3.122.239.186:5656/api/ipc/${id}`);
   dispatch({ type: 'DELETE_IPC', payload: id });
 };
 
 export const updateIPC = (id, newDescription) => async dispatch => {
   try {
-    const res = await axios.put(`http://3.122.239.186:8021/api/ipc/${id}`, { description: newDescription });
+    const res = await axios.put(`https://3.122.239.186:5656/api/ipc/${id}`, { description: newDescription });
     dispatch({
       type: 'UPDATE_IPC',
       payload: res.data
